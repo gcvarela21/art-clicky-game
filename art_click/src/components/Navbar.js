4afc84e81580aaa40bad15d14ff2e240aab0b62a
Gloria Varela <gvarela2@horizon.csueastbay.edu>
2021-03-12 15:21:38 -0800

    removed unsued elements in the navbar

--- art_click/src/components/Navbar.js
+++ art_click/src/components/Navbar.js
@@ -1,64 +1,20 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import './Navbar.css';
-import { Button } from './Button';
+
 
 
 function Navbar() {
-    // const [click, setClick] = useState(false);
-    // const [button, setButton] = useState(true);
 
-    // const handleClick = () => setClick(!click); 
-    // const closeMobileMenu = () => setClick(false);
-
-    // const showButton = () => {
-    //     if(window.innerWidth <= 960) {
-    //         setButton(false);
-    //     } else {
-    //         setButton(true);
-    //     }
-    // }; 
-    // window.addEventListener('resize', showButton);
 
 
     return (
         <>
-        <nav className='navbar'>
-            <div className='navbar-container'>
-               <Link to='/' className='navbar-logo'>
-               Clicky Game <i className='fas fa-gamepad' />
-                </Link>
-                {/* <div className='menu-icon' onClick={handleClick}>
-                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-                </div>
-                <ul clasName={click ? 'nav-menu active' : 'nav-menu'}>
-                    <li className='nav-item'>
-                        <Link to='/' className='nav-links' onClick={closeMobileMenu} >
-                            Home
-                        </Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/' className='nav-links' onClick={closeMobileMenu} >
-                            Work
-                        </Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/' className='nav-links' onClick={closeMobileMenu} >
-                            Resume
-                        </Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/' className='nav-links' onClick={closeMobileMenu} >
-                            About
-                        </Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/' className='nav-links-mobile' onClick={closeMobileMenu} >
-                            Contact
-                        </Link>
-                    </li>
-                </ul>
-                {button && <Button buttonStyle='btn--outline'>Contact</Button>} */}
+        <nav className='navbar row'>
+            <div className='col-12 navbar-container d-flex justify-content-center'>
+
+              
+                <i className='fas fa-gamepad'>Clicky Game</ i>
+               
             </div>
         </nav>
             
